46bf6e8e1a391e0c7f395b2f58bc7cd36b7053d6
Thiago Ribeiro <thiagitosouza@gmail.com>
2025-09-10 15:14:08 -0300

    feat: add ci for testing pruposes

---
+++ spec/workers/my_worker_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe MyWorker do
+  describe '#perform' do
+    it 'should output the correct message' do
+      expect { subject.perform }.to output("hello world\n").to_stdout
+    end
+    
+    it 'should be enqueued correctly' do
+      MyWorker.perform_async
+      expect(MyWorker.jobs.size).to eq(1)
+    end
+  end
+end
